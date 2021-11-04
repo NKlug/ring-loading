00089e1d4bed963189a4c3219d02e00f0dfc8d28
Nikolas Klug <klug.nikolas@googlemail.com>
2021-11-04 17:33:44 +0100

    Compute capacities according to demands across cuts

--- relaxed_ring_loading.py
+++ relaxed_ring_loading.py
@@ -1,5 +1,18 @@
 import numpy as np
 
+from symmetric_matrix import SymmetricMatrix
+
 
 def relaxed_ring_loading(n, demands):
     pass
+
+
+def compute_capacities(n, demands_across_cuts):
+    c = np.zeros((n,), dtype=demands_across_cuts.dtype)
+    m = np.max(demands_across_cuts)
+
+    for i in range(n):
+        max_tight_capacity = np.max(demands_across_cuts[:i, i] - c[:i], initial=0)
+        max_m_capacity = np.max(demands_across_cuts[i, i + 1:] - m / 2, initial=0)
+        c[i] = np.maximum(max_tight_capacity, max_m_capacity)
+    return c
