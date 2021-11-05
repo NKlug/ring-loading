e522c939f8fb5f11a4e60348eb5efd90ab133d95
Nikolas Klug <klug.nikolas@googlemail.com>
2021-11-05 13:13:14 +0100

    Add check for cut condition

--- check_cut_condition.py
+++ check_cut_condition.py
@@ -1,4 +1,7 @@
-
-
-def check_cut_condition(demands, capacities):
-    pass
+def check_cut_condition(n, demands, capacities):
+    for i in range(0, n - 1):
+        for j in range(i + 1, n):
+            if demands[i, j] > capacities[i] + capacities[j]:
+                print(f"Cut condition violated for cut {(i, j)}!")
+                return False
+    return True
