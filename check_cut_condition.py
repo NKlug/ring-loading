5587dd2f78561cfce52d1a2d5e88d4b5fddd8589
Nikolas Klug <klug.nikolas@googlemail.com>
2021-11-05 14:51:34 +0100

    Fix variable name in check cut condition

--- check_cut_condition.py
+++ check_cut_condition.py
@@ -1,7 +1,7 @@
-def check_cut_condition(n, demands, capacities):
+def check_cut_condition(n, demands_across_cuts, capacities):
     for i in range(0, n - 1):
         for j in range(i + 1, n):
-            if demands[i, j] > capacities[i] + capacities[j]:
+            if demands_across_cuts[i, j] > capacities[i] + capacities[j]:
                 print(f"Cut condition violated for cut {(i, j)}!")
                 return False
     return True
