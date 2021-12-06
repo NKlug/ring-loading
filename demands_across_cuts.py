c203f96373372c9eda8420aeb2774405b6b7a84a
Nikolas Klug <klug.nikolas@googlemail.com>
2021-12-06 22:15:53 +0100

    Add function to compute demands across cut for one edge fixed in special case of crossed contracted instance

--- demands_across_cuts.py
+++ demands_across_cuts.py
@@ -37,14 +37,45 @@ def naive_compute_demands_across_cuts(n, d):
             # loop over demands
             for k in range(0, n - 1):
                 for l in range(k + 1, n):
-                    if crosses_cut((k, l), (i, j)):
+                    if demand_crosses_cut((k, l), (i, j)):
                         x += d[k, l]
             D[i, j] = x
 
     return D
 
 
-def crosses_cut(demand, cut):
+def demands_across_cuts_edge_fixed(n, S, demands):
+    """
+    Computes the demand across cuts for all cuts of the form {k, edge} with edge - n/2 <= k < edge.
+    Furthermore assumes the special case that all demands are crossing and that the given instance is contracted.
+    :param S: sorted list of indices of remaining demands
+    :param edge:
+    :param demands:
+    :param n:
+
+    :return:
+    """
+    # assert that size is even - for contracted crossing instances, it always is
+    assert n % 2 == 0
+    demands_across_cuts = np.zeros(n // 2 - 1)
+
+    # the first cut {edge - n / 2 + 1, edge} is crossed by all remaining demands except the one we are trying to route,
+    # which is the first element of S.
+    if len(S) > 1:
+        dim_1_indices, dim_2_indices = zip(*S)
+        demands_across_cuts[0] = np.sum(demands[dim_1_indices, dim_2_indices])
+    else:
+        # if there is only one demand left, the demand across the first cut (and all other cuts) is 0.
+        demands_across_cuts[0] = 0
+
+    # assumes S to be sorted
+    for k in range(1, len(S)):
+        demands_across_cuts[k] = demands_across_cuts[k-1] - demands[S[k-1]]
+
+    return demands_across_cuts
+
+
+def demand_crosses_cut(demand, cut):
     """
     Determines whether a demand crosses a cut.
     :param demand:
